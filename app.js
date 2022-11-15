c46e5565cc5efd93010dca5f62a4193fab52bf8b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-15 19:02:35 +0100

    feat: use a helpter function for navigation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const rootDir = require("./helpers/path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -14,7 +15,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);

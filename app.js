17a4bc00786314ea1b4a50403a3d6f41a853ddb1
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-15 18:15:05 +0100

    feat: limit middleware execution to post requests

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });

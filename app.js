758b73bb7b94cb93e85f90bbbed1861bd3b07083
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-15 18:12:56 +0100

    feat: install body-parser. parse incoming requests

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from the Add-Product page!</h1>");
+  res.send(
+    "<form action='/product' method='post'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
 

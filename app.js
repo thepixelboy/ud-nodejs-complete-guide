9d36545e6f620e81fc1be0b69d57e9deb7169b95
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-16 11:12:07 +0100

    feat: serve css files statically

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const rootDir = require("./helpers/path");
+const app = express();
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);

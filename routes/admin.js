e312232bfc17547fa15a3931db15859f9fea6d17
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-18 15:05:18 +0100

    feat: converting html files to pug

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,11 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    products: products,
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
 });
 
 router.post("/add-product", (req, res, next) => {

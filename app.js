f27f5ca6545b91dc0f21ea8149f08d9fd1d6be8f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-19 17:57:44 +0100

    feat: finish the controllers

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorsController = require("./controllers/errors");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,8 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
-});
+app.use(errorsController.get404);
 
 app.listen(3000);

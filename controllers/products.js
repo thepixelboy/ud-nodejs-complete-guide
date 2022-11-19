df9bfb4ea059948a07346ae9e9ddb824ba7e1bb6
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-19 18:08:31 +0100

    feat: add a product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,20 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
-    products: products,
     pageTitle: "Add Product",
     path: "/admin/add-product",
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render("shop", {
     products: products,
     pageTitle: "Shop",

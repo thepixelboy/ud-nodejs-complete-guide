cac3e8661344fdfa99f0f91225312ce33c45fad4
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-23 18:49:57 +0100

    feat: retrieve data and find products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.findAll()
     .then((products) => {
       res.render("shop/product-list", {
         products: products,
@@ -27,7 +27,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.findAll()
     .then((products) => {
       res.render("shop/index", {
         products: products,

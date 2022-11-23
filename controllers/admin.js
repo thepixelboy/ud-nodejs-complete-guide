a26301498f56b08dda0e4d8d25118da544b9a57b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-23 18:10:46 +0100

    feat: insert data and create product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-
-  const product = new Product(null, title, imageUrl, price, description);
-
-  product
-    .save()
-    .then(() => {
-      res.redirect("/");
-    })
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => console.log("Created Product"))
     .catch((error) => console.log(error));
 };
 

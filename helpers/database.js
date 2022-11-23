3a6352c5f6c69a9e2e578ce9fe29d50ef6c6599c
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-23 15:51:48 +0100

    feat: connect to the database using sequelize

--- helpers/database.js
+++ helpers/database.js
@@ -1,11 +1,16 @@
 const mariadb = require("mariadb");
+const Sequelize = require("sequelize");
+
 require("dotenv").config();
 
-const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_USER,
+  {
+    dialect: "mariadb",
+    host: process.env.DB_HOST,
+  }
+);
 
-module.exports = pool;
+module.exports = sequelize;

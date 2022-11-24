65db8a22f25c331eef1ce6dc592f97942e7fde05
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-24 19:05:31 +0100

    feat: fetch rerlated products

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,8 +37,10 @@ exports.getEditProduct = (req, res, next) => {
 
   const prodId = req.params.productId;
 
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    .then((products) => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -75,7 +77,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then((products) => {
       res.render("admin/products", {
         products: products,

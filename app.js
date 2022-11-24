1d9580a2737c8470b4b34630045ab38fb4f387a7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-24 16:08:39 +0100

    feat: create and manage a dummy user

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => console.log(error));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -28,9 +37,19 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "John Doe", email: "john.doe@mail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {

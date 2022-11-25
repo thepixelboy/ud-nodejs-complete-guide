62c467392d35785bb00c95cdd4ea098c779ee156
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-25 14:08:20 +0100

    feat: reset the cart, fetch and output orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -106,9 +106,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -125,16 +127,25 @@ exports.postOrder = (req, res, next) => {
         .catch((error) => console.log(error));
     })
     .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
       res.redirect("/orders");
     })
     .catch((error) => console.log(error));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    pageTitle: "Your Orders",
-    path: "/orders",
-  });
+  req.user
+    .getOrders({ include: ["products"] })
+    .then((orders) => {
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((error) => console.log(error));
 };
 
 exports.getCheckout = (req, res, next) => {

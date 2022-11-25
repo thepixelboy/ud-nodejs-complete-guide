db1d40dfb342759d0b2d87ad3b77826f62d20731
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-25 13:27:40 +0100

    feat: delete related items and delete cart products

--- controllers/shop.js
+++ controllers/shop.js
@@ -91,10 +91,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((error) => console.log(error));
 };
 
 exports.getOrders = (req, res, next) => {

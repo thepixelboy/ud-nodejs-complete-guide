3f3000b5145e086a789e0f4727e3f88b8789b8b7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-25 18:52:02 +0100

    feat: create products with mongodb

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description,
-      userId: req.user.id,
-    })
+  const product = new Product(title, price, description, imageUrl);
+  product
+    .save()
     .then((result) => {
       console.log("Product Created");
       res.redirect("/admin/products");

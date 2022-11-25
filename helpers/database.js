fdc27c391a2e9d2d7f26e0f409e9b52a24207331
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-25 18:12:07 +0100

    feat: install the mongodb driver

--- helpers/database.js
+++ helpers/database.js
@@ -1,15 +1,18 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    dialect: "mariadb",
-    host: process.env.DB_HOST,
-  }
-);
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;

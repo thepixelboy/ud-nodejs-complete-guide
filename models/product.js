d197a44a1237073efbc4cfb4c0d51de68098a4a3
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-25 18:19:26 +0100

    feat: create the database connection

--- models/product.js
+++ models/product.js
@@ -1,27 +1,14 @@
-const Sequelize = require("sequelize");
+const mongoConnect = require("../helpers/database");
 
-const sequelize = require("../helpers/database");
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
 
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+  save() {}
+}
 
 module.exports = Product;

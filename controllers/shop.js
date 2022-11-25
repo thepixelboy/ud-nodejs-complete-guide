7bc5ccbd2a456fd95c14806f3a98d55a8848bee7
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-25 13:52:29 +0100

    feat: store cartitems as orderitems

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -102,6 +101,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .then((result) => {
       res.redirect("/cart");
+    })
+    .catch((error) => console.log(error));
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((error) => console.log(error));
+    })
+    .then((result) => {
+      res.redirect("/orders");
     })
     .catch((error) => console.log(error));
 };

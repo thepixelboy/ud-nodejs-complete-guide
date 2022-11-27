0e733389b16263dfb5a514141787f28ab2e9b0a1
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-27 18:24:44 +0100

    feat: work on cart items and orders

--- models/cart.js
+++ models/cart.js
@@ -1,14 +1,31 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const { getDb } = require("../helpers/database");
 
-const sequelize = require("../helpers/database");
+class Cart {
+  constructor(items, totalPrice) {
+    this.items = items;
+    this.totalPrice = totalPrice;
+  }
 
-const Cart = sequelize.define("cart", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-});
+  save() {
+    const db = getDb();
+    return db.collection("carts").insertOne(this);
+  }
+
+  static findById(cartId) {
+    const db = getDb();
+    return db
+      .collection("carts")
+      .find({ _id: new mongodb.ObjectId(cartId) })
+      .next()
+      .then((cart) => {
+        console.log(cart);
+        return cart;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
 
 module.exports = Cart;
--- models/user.js
+++ models/user.js
@@ -2,14 +2,34 @@ const mongodb = require("mongodb");
 const { getDb } = require("../helpers/database");
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const db = getDb();
+    // return db
+    //   .collection("users")
+    //   .updateOne(
+    //     { _id: new mongodb.ObjectId(this._id) },
+    //     { $push: { cart: { ...product, quantity: 1 } } }
+    //   );
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {

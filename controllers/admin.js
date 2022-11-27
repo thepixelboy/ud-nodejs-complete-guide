f73c74f39fb65c5c21cb95bd45060ae6fbf61d52
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-27 12:07:04 +0100

    refactor: change prodid

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require("mongodb");
 const Product = require("../models/product");
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -62,7 +59,7 @@ exports.postEditProduct = (req, res, next) => {
     updatedPrice,
     updatedDesc,
     updatedImageUrl,
-    new ObjectId(prodId)
+    prodId
   );
 
   product

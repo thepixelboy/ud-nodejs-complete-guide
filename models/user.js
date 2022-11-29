ee80843ea175d179176e8cee3545ca7510c07956
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-11-29 19:04:18 +0100

    feat: delete cart items

--- models/user.js
+++ models/user.js
@@ -73,6 +73,19 @@ class User {
       });
   }
 
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db

631acad28b761e5edfdc9ad261ae14c6f1edb65b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-03 11:46:06 +0100

    feat: work on the shopping cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -57,7 +57,6 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then((result) => {
-      console.log(result);
       res.redirect("/cart");
     })
     .catch((error) => console.log(error));

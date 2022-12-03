043c4bd9ea0475bcd800bca4b072b8f502b07e74
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-03 12:25:56 +0100

    feat: create and get orders

--- routes/shop.js
+++ routes/shop.js
@@ -8,11 +8,11 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
+// router.get("/orders", shopController.getOrders);
 // router.get("/checkout", shopController.getCheckout);
 
 router.post("/cart", shopController.postCart);
 router.post("/cart-delete-item", shopController.postCartDeleteProduct);
-// router.get("/orders", shopController.getOrders);
-// router.post("/create-order", shopController.postOrder);
+router.post("/create-order", shopController.postOrder);
 
 module.exports = router;

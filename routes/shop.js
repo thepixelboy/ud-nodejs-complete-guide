61af80aa70493235a5ac16373bb479516d6c3b64
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-04 11:15:29 +0100

    feat: get and display the orders

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
-// router.get("/orders", shopController.getOrders);
+router.get("/orders", shopController.getOrders);
 // router.get("/checkout", shopController.getCheckout);
 
 router.post("/cart", shopController.postCart);

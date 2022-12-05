3ee5b298d8a131b300638aa125a1aeb2fa444a28
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-05 16:32:32 +0100

    feat: set a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,15 @@
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
-  // console.log(req.session.isLoggedIn);
+  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  // res.setHeader("Set-Cookie", "loggedIn=true");
   req.isLoggedIn = true;
+  res.cookie("isLoggedIn", true);
   res.redirect("/");
 };

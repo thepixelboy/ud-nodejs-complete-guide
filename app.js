3b29f1794cf314f377cccfaea3462fada772a9f0
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-07 16:26:27 +0100

    feat: initialize the session middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorsController = require("./controllers/errors");
 const User = require("./models/user");
@@ -20,6 +21,9 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById("6388e82ddc1ca6e4768610eb")

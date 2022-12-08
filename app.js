f75bc2571199d3c7dacc738f6e78ae4c5462820f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-08 18:07:22 +0100

    feat: use mongodb to store sessions

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorsController = require("./controllers/errors");
 const User = require("./models/user");
 
-const app = express();
-
 require("dotenv").config();
+
+const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_SESSION,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -22,7 +27,12 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,10 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
+  // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
 
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 

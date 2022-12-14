fc0c77096528ac00578c767945dbbcc60c3c8847
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-14 19:01:30 +0100

    feat: reset passwords

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignup);
+router.get("/reset", authController.getReset);
 
 router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);

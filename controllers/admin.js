553bad3095bbdad9438df9546f33281159e6fd3e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-18 12:15:03 +0100

    feat: add authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -78,7 +78,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     // .populate("userId")
     .then((products) => {
       res.render("admin/products", {

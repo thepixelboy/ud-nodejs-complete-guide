163b86a9791ff810172af57fe8b99cbbe0c287dd
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-18 12:54:43 +0100

    feat: built-in and custom validators

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,15 @@ router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.post(
   "/signup",
-  check("email").isEmail().withMessage("Please enter a valid email"),
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email address is forbidden.");
+      }
+      return true;
+    }),
   authController.postSignup
 );
 router.post("/reset", authController.postReset);

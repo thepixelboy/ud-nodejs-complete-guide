47ebf15f463720cb464cf3f6586621b847783ddc
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-18 13:08:55 +0100

    feat: check for field equality

--- routes/auth.js
+++ routes/auth.js
@@ -28,6 +28,12 @@ router.post(
     )
       .isLength({ min: 5 })
       .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
   ],
   authController.postSignup
 );

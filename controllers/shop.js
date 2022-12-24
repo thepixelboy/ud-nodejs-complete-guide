d5718d4c10aee224a9cae4ba20b476479e891fb8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-24 13:21:14 +0100

    feat: prepare pagination data on the server

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,15 +42,28 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   const page = req.query.page;
+  let totalItems;
+
   Product.find()
-    .skip((page - 1) * ITEMS_PER_PAGE)
-    .limit(ITEMS_PER_PAGE)
-    .then((products) => {
-      res.render("shop/index", {
-        products: products,
-        pageTitle: "Shop",
-        path: "/",
-      });
+    .countDocuments()
+    .then((numProducts) => {
+      const totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+        .then((products) => {
+          res.render("shop/index", {
+            products: products,
+            pageTitle: "Shop",
+            path: "/",
+            totalProducts: totalItems,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page - 1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+          });
+        });
     })
     .catch((err) => {
       const error = new Error(err);

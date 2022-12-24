c8d57a5b21a84eea5adf219948f0a06a4a386437
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-24 13:13:53 +0100

    feat: retrieve a chunk of data

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -39,7 +41,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
       res.render("shop/index", {
         products: products,

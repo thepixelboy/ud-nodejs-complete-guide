f5e8e33a20368626909172e179c015fa7d6506d8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-12-26 15:22:23 +0100

    feat: add client side js code

---
+++ public/js/admin.js
@@ -0,0 +1,6 @@
+const deleteProduct = (btn) => {
+  const prodId = btn.parentNode.querySelector("[name=productId]").value;
+  const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+
+  const productElement = btn.closest("article");
+};

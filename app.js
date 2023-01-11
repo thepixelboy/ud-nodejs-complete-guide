f0e896aacdf3c6ef9cadfcbe2e33b597d5eb3b20
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-01-11 10:55:00 +0100

    feat: set secure response headers with helmet

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
 const multer = require("multer");
+const helmet = require("helmet");
 
 const errorsController = require("./controllers/errors");
 const User = require("./models/user");
@@ -46,6 +47,8 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
